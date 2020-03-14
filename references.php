3934846d3f05139ee978a2047bdd888f578e5ebc
exw4141 <exw4141@rit.edu>
2020-03-14 17:47:39 -0400

    Adding References page

---
+++ references.php
@@ -0,0 +1,106 @@
+<?php
+    $path = './';
+    $title = "References";
+    $html_url = "https://validator.w3.org/check?uri=https%3A%2F%2Fiste240-project.herokuapp.com%2Freferences.php";
+    $css_url = "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fiste240-project.herokuapp.com%2Freferences.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en";
+    $filename = "references";
+
+    include $path."assets/inc/header.php";
+?>
+
+        <table>
+            <tr>
+                <th>Link</th>
+                <th>Last Updated</th>
+                <th>Used In</th>
+                <th>Image</th>
+            </tr>
+            <tr>
+                <td><a href="https://en.wikipedia.org/wiki/Flag_of_Chicago" target="_blank">https://en.wikipedia.org/wiki/Flag_of_Chicago</a></td>
+                <td>September 20th, 2019</td>
+                <td>This is the main logo shown at the left of the navbar.</td>
+                <td><img class="ref-img" src="assets/images/illinois-flag.png" alt="Chicago Flag" title="Chicago's City Flag" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.yelp.com/biz_photos/wildberry-pancakes-and-cafe-chicago-2?select=K3csNXWAXYUxdTQ25ZoYFw" target="_blank">https://www.yelp.com/biz_photos/wildberry-pancakes-and-cafe-chicago-2?select=K3csNXWAXYUxdTQ25ZoYFw</a></td>
+                <td>September 13th, 2014</td>
+                <td>This is the image next to the description of Wildberry's on the Dining page.</td>
+                <td><img class="ref-img" src="assets/images/wildberrys.jpg" alt="Wildberry's" title="Wildberry's" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.yelp.com/biz/crisp-chicago?start=160" target="_blank">https://www.yelp.com/biz/crisp-chicago?start=160</a></td>
+                <td>Not Found</td>
+                <td>This is the image next to the description of Crisp on the Dining page.</td>
+                <td><img class="ref-img" src="assets/images/crisp.jpg" alt="Crisp" title="Crisp's Fried Chicken" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.enjoyillinois.com/explore/listing/900-north-michigan-shops" target="_blank">https://www.enjoyillinois.com/explore/listing/900-north-michigan-shops</a></td>
+                <td>2019</td>
+                <td>This is the background image used in the banner on the Shopping page.</td>
+                <td><img class="ref-img" src="assets/images/900-michigan.jpg" alt="900 North Michigan Shops" title="900 North Michigan Shops" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.themagnificentmile.com/explore/listing/900-north-michigan-shops" target="_blank">https://www.enjoyillinois.com/explore/listing/900-north-michigan-shops</a></td>
+                <td>2019</td>
+                <td>This is the image next to the description of Magnificent Mile on the Shopping page.</td>
+                <td><img class="ref-img" src="assets/images/900-michigan-building.jpg" alt="900 North Michigan Shops" title="900 North Michigan Shops" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.eataly.com/us_en/stores/chicago/chicago-store-tours/" target="_blank">https://www.eataly.com/us_en/stores/chicago/chicago-store-tours/</a></td>
+                <td>2019</td>
+                <td>This is the image next to the description of Eataly on the Shopping page.</td>
+                <td><img class="ref-img" src="assets/images/eataly.jpg" alt="Eataly" title="Eataly" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.usatoday.com/picture-gallery/travel/experience/2019/08/09/chicago-duck-derby-floats-63-000-rubber-ducks-help-special-olympics/1965048001/" target="_blank">https://www.usatoday.com/picture-gallery/travel/experience/2019/08/09/chicago-duck-derby-floats-63-000-rubber-ducks-help-special-olympics/1965048001/</a></td>
+                <td>August 9th, 2019</td>
+                <td>This is the background image used in the banner on the Events page.</td>
+                <td><img class="ref-img" src="assets/images/ducky-derby.jpg" alt="Ducky Derby" title="Ducky Derby" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.prnewswire.com/news-releases/63-000-rubber-ducks-will-splash-into-the-chicago-river-aug-8-for-special-olympics-illinois-300896794.html" target="_blank">https://www.prnewswire.com/news-releases/63-000-rubber-ducks-will-splash-into-the-chicago-river-aug-8-for-special-olympics-illinois-300896794.html</a></td>
+                <td>August 6th, 2019</td>
+                <td>This is the image next to the description of the Ducky Derby on the Events page.</td>
+                <td><img class="ref-img" src="assets/images/ducky-derby2.jpg" alt="Ducky Derby" title="Ducky Derby" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://edmidentity.com/2019/07/09/lollapalooza-2019-afterparties/" target="_blank">https://edmidentity.com/2019/07/09/lollapalooza-2019-afterparties/</a></td>
+                <td>July 9th, 2019</td>
+                <td>This is the image next to the description of Lollapalooza on the Events page.</td>
+                <td><img class="ref-img" src="assets/images/lollapalooza.jpg" alt="Lollapalooza" title="Lollapalooza" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.nbcchicago.com/blogs/worth-the-trip/taste-of-chicago-2019-lineup-509061481.html" target="_blank">https://edmidentity.com/2019/07/09/lollapalooza-2019-afterparties/</a></td>
+                <td>April 5th, 2019</td>
+                <td>This is the image next to the description of the Taste of Chicago festival on the Events page.</td>
+                <td><img class="ref-img" src="assets/images/taste-of-chicago.png" alt="Taste of Chicago" title="Taste of Chicago" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://en.wikipedia.org/wiki/HTML" target="_blank">https://en.wikipedia.org/wiki/HTML</a></td>
+                <td>September 15th, 2019</td>
+                <td>This is the HTML logo shown in the footer.</td>
+                <td><img class="ref-img" src="assets/images/html.png" alt="HTML Logo" title="HTML Validator" /></td>
+            </tr>
+            <tr>
+                <td><a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets" target="_blank">https://en.wikipedia.org/wiki/Cascading_Style_Sheets</a></td>
+                <td>September 27th, 2019</td>
+                <td>This is the CSS logo shown in the footer.</td>
+                <td><img class="ref-img" src="assets/images/css.png" alt="CSS Logo" title="CSS Validator" /></td>
+            </tr>
+            <tr>
+                <td>Note: All other photos were taken by me.</td>
+                <td>N/A</td>
+                <td>N/A</td>
+                <td>N/A</td>
+            </tr>
+            <tr>
+                <td><a href="https://www.lpzoo.org/animals" target="_blank">https://www.lpzoo.org/animals</a></td>
+                <td>Not Found</td>
+                <td>Information from this site was used in my description for the Lincoln Park Zoo on the Attractions page.</td>
+                <td>N/A</td>
+            </tr>
+        </table>
+
+    <?php
+        include $path."assets/inc/footer.php";
+    ?>
